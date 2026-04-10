7a71e8ac4d40784d63149c14e621e0b6e64c91bd
Whitman 208 <wh208@it.farmingdale.edu>
2026-04-10 14:20:42 -0400

    ive rewritten the code to better match our new prompt as the one previous was outdated

--- Employee.java
+++ Employee.java
@@ -1,7 +1,21 @@
 import java.sql.*;
 
-public class BooksDB{
+public class Employee {
   public static void main(String[] args){
+
+    String url = "jdbc:mysql://localhost:3306/employeesdb";
+    String selectQuery = "select * from employees";
+
+
+
+    try{ 
+      Connection connection = DriverManager.getConnection(url, "root", "password");
+      Statement statement = connection.createStatement()
+
+
+    }catch(SQLException e){
+      throw new RuntimeException(e);
+    }
 
 
     
